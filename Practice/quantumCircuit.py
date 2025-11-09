b56018ed5d1bee3c5d81ce68dc4b6208c20f53bc
Dani <danielteres@correo.ugr.es>
2025-11-09 23:59:04 +0100

    Ejercicio banco qml

--- Practice/quantumCircuit.py
+++ Practice/quantumCircuit.py
@@ -1,21 +1,7 @@
 from qiskit import QuantumCircuit, transpile
+from qiskit.circuit import ParameterVector
 import numpy as np
 from qiskit_aer import AerSimulator
-
-def exQCAndSim():
-    qc = QuantumCircuit(1)
-    qc.h(0)
-    qc.z(0)
-    qc.h(0)
-    qc.measure_all()
-
-    sim = AerSimulator()
-    # transpile convierte todo el circuito a "código máquina".
-    # El .run envía un job, pero no interesa nada 
-    results = sim.run(transpile(qc), shots=10).result()
-    counts = results.get_counts()
-    print(counts)
-
 
 def simPrintVals(qc, shotsRun: int=1, text: str="result simulation"):
     sim = AerSimulator()
@@ -23,29 +9,12 @@ def simPrintVals(qc, shotsRun: int=1, text: str="result simulation"):
     cnt = results.get_counts()
     print(text, next(iter(cnt)))
 
-def resetQc(qc):
-    # Reset all qubits to |0> at the start of the iteration
-    for q in range(qc.num_qubits):
-        qc.reset(q)
 
-def applyLogicGate(qc, init_vals, gate_func):
-    for i, j in init_vals:
-        resetQc(qc)
-        # Initialize qubits 0 and 1
-        qc.ry(i, 0)
-        qc.ry(j, 1)
-
-        # Apply specific gate
-        gate_func(qc)
-
-        # Measure qubit 2 in bit 0
-        qc.measure(2, 0)
-        simPrintVals(qc, text=f"{1 if i == np.pi else 0} {1 if j == np.pi else 0} --> ")
-
-
-def gatesAND(qc):
+def gatesAND():
     # Use CCNOT to verify both states are |1>
+    qc = QuantumCircuit(3, 1)
     qc.ccx(0, 1, 2)
+    return qc
 
 def gatesOR(qc):
     # Flip qubit 2 if one of the inputs it's |1>.
@@ -59,28 +28,24 @@ def gatesXOR(qc):
     qc.cx(0, 1)
     qc.cx(1, 2)
 
-def andGate(qc, init_vals):
-    applyLogicGate(qc, init_vals, gatesAND)
-
-def orGate(qc, init_vals):
-    applyLogicGate(qc, init_vals, gatesOR)
-
-def xorGate(qc, init_vals):
-    applyLogicGate(qc, init_vals, gatesXOR)
 
 if __name__ == "__main__":
     # Use preety print for arrays
     np.set_printoptions(formatter={'complex_kind': lambda x: f"{x:10.4g}"})
-    # exQCAndSim()
-
-    init_vals = [[0, 0], [0, np.pi], [np.pi, 0], [np.pi, np.pi]]
+    init_vals = [[0, 0], [0, 1], [1, 0], [1, 1]]
     qc = QuantumCircuit(3, 1)
+    qc.h(0)
+    qc.h(1)
+    # Apply instructions from one circuit to the other circuit specified in this
+    # function
+    qc = qc.compose(gatesAND())
+    qc.save_statevector()
+    
+    sim = AerSimulator()
+    qct = transpile(qc, sim)
+    sv = sim.run(qct, shots=1).result().get_statevector()
 
-    print("AND gate simulated:")
-    andGate(qc, init_vals)
+    for ket in sv.to_dict():
+        # Ya que está en little endian
+        print(ket[::-1])
 
-    print(f"\nXOR gate simulated:")
-    xorGate(qc, init_vals)
-
-    print(f"\nOR gate simulated:")
-    orGate(qc, init_vals)
